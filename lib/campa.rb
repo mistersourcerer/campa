58befbcc0d7e1edc3e96a2ec002a853bd274dbe6
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2021-06-14 16:37:06 -0300

    Bring zeitwerk to the party.

--- lib/campa.rb
+++ lib/campa.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 require_relative "campa/version"
+require "zeitwerk"
 
 module Campa
   class Error < StandardError; end
   # Your code goes here...
 end
+
+loader = Zeitwerk::Loader.for_gem
+loader.setup
